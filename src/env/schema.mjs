2b64897d0b81cc81a5766b9afbe75ad8b8251cc2
Raihan Adam <hallwack.id@gmail.com>
2023-01-09 00:22:54 +0700

    feat: add supabase env to schema env

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -30,6 +30,8 @@ export const serverSchema = z.object({
  */
 export const clientSchema = z.object({
   // NEXT_PUBLIC_CLIENTVAR: z.string(),
+  NEXT_PUBLIC_SUPABASE_URL: z.string(),
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string()
 });
 
 /**
@@ -40,4 +42,6 @@ export const clientSchema = z.object({
  */
 export const clientEnv = {
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 };
