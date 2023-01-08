a03e0aaf5790fc6fa2259f8bc295b617acaec53f
Raihan Adam <hallwack.id@gmail.com>
2023-01-08 16:15:04 +0700

    fix: add prisma middleware

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import argon2 from "argon2";
 
 import { env } from "../env/server.mjs";
 
@@ -15,5 +16,35 @@ export const prisma =
   });
 
 if (env.NODE_ENV !== "production") {
+  /* hash password when create user */
+  prisma.$use(async (params, next) => {
+    if (params.model === 'User' && (params.action === 'create')) params.args.data.password = await argon2.hash(params.args.data.password)
+    return next(params)
+  })
+
+  /* verify password when login */
+  prisma.$use(async (params, next) => {
+    if (params.model === "User" && params.action === "findFirst") {
+      const password = params.args.where?.password
+      if (password) {
+        delete params.args.where.password
+      }
+      const user = await next(params)
+      if (password) {
+        try {
+          if (user && await argon2.verify(user.password, password)) {
+            return user
+          }
+        } catch (error) {
+          return null
+        }
+        return null
+      } else {
+        return user
+      }
+    }
+    return next(params)
+  })
+
   global.prisma = prisma;
 }
